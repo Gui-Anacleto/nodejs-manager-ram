4b9b85b931593a182228656011e98ef37b06c14e
Paquista0 <guilherme_fa@outlook.com>
2022-01-21 17:44:34 -0300

    pcusage ram

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,12 +1,14 @@
 const os = require('os');
 
 const {arch, platform, totalmem, freemem} = os;
+const tRam = totalmem() /1024 /1024;
+const fRam = freemem() /1024 /1024;
 
 const stats = {
     OS: platform(),
     arch: arch(),
-    totalram: totalmem(),
-    freeram: freemem(),
-}
+    totalram: parseInt(tRam),
+    freeram: parseInt(fRam),
+};
 
 console.log(stats);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const os = require('os');
 const host = 'http://localhost';
 const port = 3000;
 
 http.createServer((req, res) => {
     res.end('<h1>TESTE</h1>')
 }).listen(port, () => console.log(`Server is running in ${host}:${port}`));
+
